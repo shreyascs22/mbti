74c806ee05ac31ca4ffe8d335bbb7f72bb56246c
shreyascs22 <csshreyas22@gmail.com>
2023-10-12 13:28:36 +0530

    types

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function personality_functions(){
-    document.getElementById("ptypes").innerHTML=w;
-}
+// Get references to all buttons and the box
+const openButtons = document.querySelectorAll(".openButton");
+const myBox = document.getElementById("myBox");
+const closeButton = document.getElementById("closeButton");
+const modalContent = document.getElementById("modalContent");
+
+// Event listener to open the box for all buttons
+openButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    myBox.style.display = "block";
+    const content = button.getAttribute("data-content");
+    modalContent.textContent = content;
+  });
+});
+
+// Event listener to close the box
+closeButton.addEventListener("click", function() {
+  myBox.style.display = "none";
+});
